be61168b1555464387954e8c3c710677102f8920
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-08 23:38:20 +0900

    erase router

--- src/App.tsx
+++ src/App.tsx
@@ -25,31 +25,29 @@
 
 // export default App;
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      < div className="App" >
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Routes>
-            <Route path="/settings/account">aaaa</Route>
-          </Routes>
-        </header>
-      </div >
-    </Router>
+    < div className="App" >
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Routes>
+          <Route path="/settings/account">aaaa</Route>
+        </Routes>
+      </header>
+    </div >
   );
 }
 
