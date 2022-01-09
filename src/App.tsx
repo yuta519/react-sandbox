845a1d95c2c04fc76befe627259a1ea465cd7272
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-09 23:37:29 +0900

    using router

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,21 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+function Hoge() {
+  return (
+    <p>aaa</p>
+  )
+}
 
 function App() {
   return (
     < div className="App" >
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
         <Routes>
-          <Route path="/settings/account">aaaa</Route>
+          <Route path="/hoge" element={<Hoge />} >
+          </Route>
         </Routes>
-      </header>
+      </Router>
     </div >
   );
 }
