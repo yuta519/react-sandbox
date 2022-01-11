b2a9577b39711143140722de1a752bc4b46b6479
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-01-11 09:29:12 +0900

    just added data

--- src/components/ReactDnD/index.tsx
+++ src/components/ReactDnD/index.tsx
@@ -1,8 +1,23 @@
 import React from "react"
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
+
+import { CHARACTERS } from "./charactersData"
 
 export const ReactDnD: React.FC = () => {
-  const horse = "♘"
+  const horse: string = "♘"
   return (
-    <span>{horse}</span>
+    <DndProvider backend={HTML5Backend}>
+      <span>{horse}</span>
+      <ul>
+        {CHARACTERS.map(({ id, name }) => {
+          return (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          )
+        })}
+      </ul>
+    </DndProvider>
   )
 }
